83915f26a3263ecffce10875c68191f46963cfd4
karak <karak97@gmail.com>
2021-01-16 11:23:55 +0900

    テンプレートから作成。

---
+++ EFCoreTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EFCoreTest.Models;
+
+namespace EFCoreTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
